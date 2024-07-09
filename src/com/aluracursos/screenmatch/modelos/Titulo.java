cfc328bbd2319c9e5c2f1ae9e20d66118dbcf33c
juanpirxn <juanpablocarmon123@gmail.com>
2024-07-09 13:05:17 -0500

    Se agrega el proyecto

---
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -0,0 +1,62 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+
+    private double sumaDeEvaluaciones;
+    private int totalDeLasEvaluaciones;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDeLasEvaluaciones(){
+        return totalDeLasEvaluaciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void muestraFichaTecnica() {
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duración en minutos: " + getDuracionEnMinutos());
+    }
+
+    public void evalua(double nota){
+        sumaDeEvaluaciones += nota;
+        totalDeLasEvaluaciones ++;
+    }
+
+    public double calculaMedia(){
+        return sumaDeEvaluaciones / totalDeLasEvaluaciones;
+    }
+}
